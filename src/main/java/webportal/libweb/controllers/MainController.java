6b9c73f2dc4fb5bb9e0baee5eee81fe9fa78651c
Damir <damirmuhlisov2014@gmail.com>
2024-04-01 20:03:49 +0400

    secure access

--- src/main/java/webportal/libweb/controllers/MainController.java
+++ src/main/java/webportal/libweb/controllers/MainController.java
@@ -9,22 +9,38 @@ import webportal.libweb.services.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/main")
 public class MainController {
 
     private final BookService service;
 
-    @GetMapping
+    @GetMapping("/main")
     public String mainPageViewer(Model model) {
         model.addAttribute("listBooks", service.findAllBook());
         return "mainPage";
     }
     
+    @GetMapping("/login")
+    public String toLogin(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loggedIn(){
+        return "redirect:/main";
+    }
+
+    @GetMapping("/order")
+    public String getOrderDescription() {
+        return "order";
+    }
+
+
     
 }
