bc8850356e7d77667bb672976e5f0efa49db9e43
Damir <damirmuhlisov2014@gmail.com>
2024-04-18 21:12:39 +0400

    some edits in Get: homePage

--- src/main/java/webportal/libweb/controllers/MainController.java
+++ src/main/java/webportal/libweb/controllers/MainController.java
@@ -5,12 +5,12 @@ import lombok.AllArgsConstructor;
 import webportal.libweb.Book.Book;
 import webportal.libweb.Book.BookService;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,11 +24,13 @@ public class MainController {
 
     @GetMapping
     public String homePage(@RequestParam(name = "title", required = false, defaultValue = "") String title, Model model) {
-        if (title.equals("")){
-            model.addAttribute("bookList", bookService.getRandomBooks());
+        List<Book> bookList;
+        if (title.isEmpty()){
+            bookList = bookService.getRandomBooks();
         } else {
-            model.addAttribute("bookList", bookService.findByTitle(title));
+            bookList = bookService.findByTitle(title);
         }
+        model.addAttribute("bookList", bookList);
         return "home";
     }
 
@@ -36,9 +38,9 @@ public class MainController {
     public String home_searchingBooks(@RequestParam(name = "title", required = false, defaultValue = "") String title) {
         return "redirect:/?title="+title;
     }
-    
-    @GetMapping("/book/{id}")
-    public String viewBookPage(@PathVariable("id") Long id, Model model) {
+
+    @GetMapping("/book/")
+    public String viewBookPage(@RequestParam(name="id", required = true, defaultValue = "-1") Long id, Model model) {
         Optional<Book> entity = bookService.findById(id);
         if (entity.isEmpty()){
             return "noContent";
