96f327da87b183597da8b31071815c631ad36caa
Damir <damirmuhlisov2014@gmail.com>
2024-04-21 00:23:07 +0400

    Update BookAddDTO.java

--- src/main/java/webportal/libweb/DTOs/BookAddDTO.java
+++ src/main/java/webportal/libweb/DTOs/BookAddDTO.java
@@ -1,10 +1,7 @@
 package webportal.libweb.DTOs;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import lombok.Setter;
-import webportal.libweb.Author.Author;
 
 @Setter
 public class BookAddDTO {
@@ -25,12 +22,7 @@ public class BookAddDTO {
         return this.quantity;
     }
 
-    public List<Author> getAuthors(){
-        System.out.println(this.quantity);
-        List<Author> list = new ArrayList<>();
-        for (String authorName : this.authors.split(";")) {
-            list.add(new Author(authorName.trim()));
-        }
-        return list;
+    public String getAuthors(){
+        return this.authors;
     }
 }
