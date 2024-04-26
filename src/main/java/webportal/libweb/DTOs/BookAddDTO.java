e6d23106affe7637c64113465389935e3c252a57
Damir <damirmuhlisov2014@gmail.com>
2024-04-26 23:33:22 +0400

    small changes

--- src/main/java/webportal/libweb/DTOs/BookAddDTO.java
+++ src/main/java/webportal/libweb/DTOs/BookAddDTO.java
@@ -1,28 +1,15 @@
 package webportal.libweb.DTOs;
 
 
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 public class BookAddDTO {
+    private Long id;
     private String title;
     private int year;
     private int quantity;
     private String authors;
-
-    public String getTitle(){
-        return this.title;
-    }
-
-    public int getYear(){
-        return this.year;
-    }
-
-    public int getQuantity(){
-        return this.quantity;
-    }
-
-    public String getAuthors(){
-        return this.authors;
-    }
 }
